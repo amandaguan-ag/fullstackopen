f102b986a83a599fb643b383bc4839803ee93aee
amandaguan-ag <amandaguan1314@gmail.com>
2024-03-30 10:31:00 -0700

    1.7 Refactor App component to include feedback statistics

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
   // save clicks of each button to its own state
@@ -6,7 +6,37 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  const total = good + neutral + bad;
+  const average = (good * 1 + neutral * 0 + bad * -1) / (total || 1);
+  const positivePercentage = (good / (total || 1)) * 100;
+
+  const handleGoodClick = () => {
+    setGood(good + 1);
+  };
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1);
+  };
+
+  const handleBadClick = () => {
+    setBad(bad + 1);
+  };
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={handleGoodClick}>good</button>
+      <button onClick={handleNeutralClick}>neutral</button>
+      <button onClick={handleBadClick}>bad</button>
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {average.toFixed(2)}</p>
+      <p>positive {positivePercentage.toFixed(2)} %</p>
+    </div>
+  );
 };
 
 export default App;
